4d23cad9463001183f4e967ced13f59ceb7c3cc2
Unchanted <omkkdwivedi@gmail.com>
2024-10-13 02:39:38 +0530

    feat(login): added login page

--- bid_bit/src/client/App.jsx
+++ bid_bit/src/client/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Playground from "./pages/code";
 import Home from "./pages/Home.jsx";
 import React from "react";
+import Login from "./pages/Login.jsx";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/problem" element={<Playground />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </>
