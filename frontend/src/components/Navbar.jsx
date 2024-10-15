a8cb2cea4bdd46e0e9654246b09493e862f95500
Om Lanke <oplanke@gmail.com>
2024-10-15 12:12:49 +0530

    remove hamburger

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,10 @@
-import { AlignJustify } from "lucide-react";
-
 const NavbarWithDock = () => {
   return (
     <div className="fixed top-0 left-0 right-0 bg-transparent py-4 pt-6 z-10 backdrop-blur-sm">
       <div className="flex items-center justify-between px-5">
-        <div className="flex ml-3 items-center">
-          <AlignJustify className="mr-2" color="white" />
+        <div className="flex flex-col ml-3 items-center">
           <span className="text-white text-lg font-semibold">Bid By Bit</span>
+          <span className="text-white text-xs">By CSI-KSKCE</span>
         </div>
       </div>
     </div>
